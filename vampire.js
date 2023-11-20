37178e3cdd8dfd95bad281622197c043dba82e50
tajjav <taj.jav@hotmail.com>
2023-11-19 23:40:29 -0700

    add addOffspring, numberOfOffspring and isMoreSeniorThan function definitions

--- vampire.js
+++ vampire.js
@@ -10,22 +10,40 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampiresToOriginal = 0;
+    let currentVampire = this;
+    while(currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampiresToOriginal++;
+    }
+    return numberOfVampiresToOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    if (this.creator === null) {
+      return true;
+    }
+    if (vampire.creator <= this.creator) {
+      return false;
+    }
+  
+    if (this.creator < vampire.creator) {
+      return true;
+    }
 
+    return false;
   }
 
   /** Stretch **/
