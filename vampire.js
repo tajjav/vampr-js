3ae9c2fb6768277f1462e85b3bd2b6007ee75da4
tajjav <taj.jav@hotmail.com>
2023-11-20 15:22:40 -0700

    added all the methods to traverse ancestory of a vampire

--- vampire.js
+++ vampire.js
@@ -56,7 +56,49 @@ class Vampire {
   closestCommonAncestor(vampire) {
 
   }
-}
 
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    let vampire = null;
+    if (this.name === name) {
+      vampire = this;
+    }
+    for(let child of this.offspring) {
+      if (vampire) {
+        vampire = vampire
+      } else {
+        vampire = child.vampireWithName(name);
+      }
+    }
+    return vampire;
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let totalDescendents = 0;
+    for(let child of this.offspring) {
+      totalDescendents += child.totalDescendents;
+      totalDescendents++;
+    }
+    return totalDescendents;
+  }
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let results = [];
+
+    if(this.yearConverted > 1980) {
+      results.push(this);
+    }
+
+    for (let child of this.offspring) {
+      const answer = child.allMillennialVampires;
+        results.push(...answer);
+    }
+
+    return results;
+  }
+
+}
 module.exports = Vampire;
 
